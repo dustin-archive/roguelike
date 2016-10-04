d98d0fb73e2a0a0b15e5647dc589bffb2aa91898
Dustin Dowell <dustindowell22@gmail.com>
2016-10-04 10:45:27 -0500

    move badguys logic into badguys mixin

--- src/index.js
+++ src/index.js
@@ -19,8 +19,6 @@ new Vue({
     //   '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#',
     //   '#', '#', '#', '#', '+', '#', '#', '#', '#'
     // ],
-
-    badguys: []
   },
   methods: {
     mapName: function () {
@@ -30,41 +28,6 @@ new Vue({
       // there's a better way to update the map than this - we'll do it in the world computed property
       var map = this.neutral;
       this.neutral = map;
-    },
-    generate_badguys: function () {
-      var amount = 1000
-
-
-      // generate code here
-
-      this.badguys = null // replace null with an array of objects
-
-      // this will be the structure of the generated code
-      // this is temporary data (more like a story to me)
-      // entities: [
-      //   {
-      //     id: 1,
-      //     sprite: '@', // goodguy greg yay :D ggg (aka gennady gennadyevich golovkin)
-      //     bind: 'player'
-      //   },
-      //   {
-      //     id: 2,
-      //     sprite: 'w', // badguy bruce boo :C bbb
-      //     location: {
-      //       x: 2,
-      //       y: 2,
-      //       z: 0
-      //     },
-      //     health: 1000,
-      //     stamina: 40
-      //   }
-      // ],
-    },
-
-    behavior_badguys: function () {
-      // if player is within x radius move postition of badguy 1 block closer to player
-      // if touching player, attack player
-      // upon death of baguy remove badguy from badguys array
     }
   },
   computed: {
@@ -115,7 +78,7 @@ new Vue({
     // callback && callback.call(this);
   },
   mixins: [
-    // require('mixins/badguys')
+    require('mixins/badguys')
   ],
   components: {
     game: require('components/game'),
