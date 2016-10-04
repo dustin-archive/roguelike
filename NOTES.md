aaa0cdd1c7429bd38f0d621e4794fa49665fafbe
Dustin Dowell <dustindowell22@gmail.com>
2016-10-03 21:56:13 -0500

    update

---
+++ NOTES.md
@@ -0,0 +1,19 @@
+
+# world array structure
+each array item is a layer
+
+world: [
+ [],
+ [],
+ [],
+],
+
+# world sprites / blocks
+■ wall
+| door
+- door
+□ ?
+▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ◧ ◨ ◩ ◪ ◫ ◰ ◱ ◲ ◳
+
+# world entities
+@ player
--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 new Vue({
   el: '#app',
   data: {
-    app: 'hello world!'
-  },
-  watch: {
+    test: 'hello world!',
 
-  },
-  mixins: {
+		health: 100,
+		level: 0, // start in a neutral room
+    coordinates: { x: 0, y: 0, z: 0 },
 
-  }
+    // default world layer
+    neutral: [
+      '■ ■ ■ ■ - ■ ■ ■ ■',
+      '■               ■',
+      '■               ■',
+      '|       @       |',
+      '■               ■',
+      '■               ■',
+      '■ ■ ■ ■ - ■ ■ ■ ■'
+    ],
+
+		entities: [
+			{id: 1, coordinates: { x: 0, y: 0, z: 0 }}
+		]
+  },
   methods: {
+    nameMaker: function () {
 
+    }
   },
   computed: {
-
+    world: function () {
+      return null
+    }
   },
   components: {
-
+    hud: {
+      data: function () {
+        props: ['health']
+      }
+    },
+    gradient: {
+      data: function () {
+        props: ['distance']
+      }
+    },
+    mini: {
+      data: function () {
+        props: ['coordinates']
+      }
+    }
   }
 })
